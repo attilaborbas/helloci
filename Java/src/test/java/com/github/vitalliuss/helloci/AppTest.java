f981306b0bd73cba366a161bb57d325b8f6dcd44
Attila_Borbas <attila_borbas@epam.com>
2016-12-13 13:27:07 +0100

    remove testcase

--- Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java
+++ Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java
@@ -16,11 +16,6 @@ public class AppTest {
 		assertTrue(true);
 	}
 
-	@Test
-	public void testShoudBePassed_2() {
-		assertTrue(true);
-	}
-
 	@Ignore("Not implemented yet")
 	@Test
 	public void testShouldBeSKipped() {
